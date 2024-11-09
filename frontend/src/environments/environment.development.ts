d25ea01ddfdbaee377a62851e7b1b7db6c9afc7a
MatyManzur <mmanzur@itba.edu.ar>
2024-11-09 17:51:49 -0300

    feat: bookmark bundles

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrl: "https://grz0a19bkg.execute-api.us-east-1.amazonaws.com",
-  cognitoClientId: "5ga79ulbh87ofpgdlhlqfdbcq0",
-  cognitoUserPoolId: "us-east-1_dfbE2vQP4",
+  apiUrl: "https://n2yn612nee.execute-api.us-east-1.amazonaws.com",
+  cognitoClientId: "1o6f34pd2dd5su7qm2sjo7vck",
+  cognitoUserPoolId: "us-east-1_ZktITOmr0",
   region: 'us-east-1',
 };
